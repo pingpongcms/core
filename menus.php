0b516b0ded3f514346a4814dc456a45ba3013589
Gravitano <gravitano16@gmail.com>
2015-06-11 10:34:12 +0700

    enable menu ordering

--- menus.php
+++ menus.php
@@ -4,6 +4,7 @@ Menu::create('sidebar', function ($menu)
 {
 	$menu->setPresenter('Cms\Core\Presenters\Menus\SidebarPresenter');
 	$menu->setPrefixUrl(cms()->prefix());
+	$menu->enableOrdering();
 	$menu->header('MAIN NAVIGATION');
 	$menu->url('/', 'Dashboard', 1, ['icon' => 'fa fa-dashboard']);
 	$menu->url('/users', 'Users', 2, ['icon' => 'fa fa-users']);
