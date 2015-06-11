d32eb4c53ec82b087233e269c71288a30e714d20
Gravitano <gravitano16@gmail.com>
2015-06-11 10:16:20 +0700

    Added menu order

--- menus.php
+++ menus.php
@@ -5,8 +5,8 @@ Menu::create('sidebar', function ($menu)
 	$menu->setPresenter('Cms\Core\Presenters\Menus\SidebarPresenter');
 	$menu->setPrefixUrl(cms()->prefix());
 	$menu->header('MAIN NAVIGATION');
-	$menu->url('/', 'Dashboard', ['icon' => 'fa fa-dashboard']);
-	$menu->url('/users', 'Users', ['icon' => 'fa fa-users']);
+	$menu->url('/', 'Dashboard', 1, ['icon' => 'fa fa-dashboard']);
+	$menu->url('/users', 'Users', 1, ['icon' => 'fa fa-users']);
 	$menu->dropdown('Settings', function ($sub)
 	{
 		$sub->url('settings', 'General');
