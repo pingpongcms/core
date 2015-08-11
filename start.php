6240a054e41562611960b59a940499d785749821
Gravitano <gravitano16@gmail.com>
2015-08-12 01:15:22 +0700

    remove listener

--- start.php
+++ start.php
@@ -17,15 +17,3 @@ require __DIR__ . '/middlewares.php';
 require __DIR__ . '/helpers.php';
 
 require __DIR__ . '/menus.php';
-
-Event::listen('modules.settings.boot', function ()
-{
-	$menu = Menu::instance('sidebar');
-
-	$menu->whereTitle('Settings', function ($sub)
-	{
-		if (!is_null($sub)) {
-			$sub->url('administrator/settings/modules', 'Modules', 3, ['icon' => 'fa fa-circle-o']);
-		}
-	});
-});
